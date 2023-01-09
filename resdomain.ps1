431bc124591cdc547b148cc78bf5ef85d78eedb1
Thaylon Roberto Muniz da Silva <95652244+Thayyllon@users.noreply.github.com>
2023-01-09 14:45:26 -0300

    Resdomain
    Simples script em ps1 que verifica o servidor, métodos permitidos e busca domínios em paginas web.

---
+++ resdomain.ps1
@@ -0,0 +1,13 @@
+﻿#Buscador de dominios em pagina web
+
+$site = Read-Host "Digite o site"
+$web = Invoke-WebRequest -uri "$site" -Method Options
+echo "O servicor roda:"
+$web.headers.server
+echo ""
+echo "O servidor aceita os metodos"
+$web.Headers.Allow
+$web2 = Invoke-WebRequest -uri "$site"
+echo "Links encontrados no site:"
+echo ""
+$web2.links.href | Select-String http:// 
